da0f2715edfdf36d06541f4488a66f7084c9d1c4
Sahlot02 <40887789+Sahlot02@users.noreply.github.com>
2021-07-13 01:31:01 +0530

    Add files via upload

---
+++ TelCo.ColorCoder/Models.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+namespace TelCo.ColorCoder
+{
+    class ColorMap
+    {
+        protected readonly static Color[] colorMapMajor;
+        protected readonly static Color[] colorMapMinor;
+
+        static ColorMap()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+    }
+
+    public class ColorPair
+    {
+        public Color majorColor { get; set; }
+        public Color minorColor { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+        }
+    }
+}
