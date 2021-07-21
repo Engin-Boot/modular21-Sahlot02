6b9b835eeea79f9e06c6aca5cbb39a4753d7fc29
Sahlot02 <40887789+Sahlot02@users.noreply.github.com>
2021-07-21 11:29:31 +0530

    Update Environment.cs

--- TelCo.ColorCoder/Environment.cs
+++ TelCo.ColorCoder/Environment.cs
@@ -1,7 +1,5 @@
-﻿using System;
+using System;
 using System.Diagnostics;
-using System.Drawing;
-
 namespace TelCo.ColorCoder
 {
     public interface Ienvironment
@@ -11,26 +9,22 @@ namespace TelCo.ColorCoder
 
     public class test:Ienvironment
     {
-        public void print()
-        {
+        public void print(){
             int totalMajorColor = ColorMap.colorMapMajor.Length;
             int totalMinorColor = ColorMap.colorMapMinor.Length;
             Debug.Assert(totalMajorColor == 5);
             Debug.Assert(totalMinorColor == 5);
             int numricValueOfColorPair = 1;
             System.Console.WriteLine("    Testing Color Coding Manual     ");
-            System.Console.WriteLine("Number  Major Color  Minor Color");
-            for (int i = 0; i < totalMajorColor; i++)
-            {
-                for (int j = 0; j < totalMinorColor; j++)
-                {
+            for (int i = 0; i < totalMajorColor; i++){
+                for (int j = 0; j < totalMinorColor; j++){
                     ColorPair color = new ColorPair { majorColor = ColorMap.colorMapMajor[i], minorColor = ColorMap.colorMapMinor[j] };
                     int correctPairNumber = getNumber.GetPairNumberFromColor(color);
                     Debug.Assert(numricValueOfColorPair == correctPairNumber);
                     numricValueOfColorPair++;
                 }
             }
-            Debug.Assert(numricValueOfColorPair == 25);
+            Debug.Assert(numricValueOfColorPair-1 == 25);
         }
     }
 
@@ -40,20 +34,16 @@ namespace TelCo.ColorCoder
         int totalMinorColor = ColorMap.colorMapMinor.Length;
         int numricValueOfColorPair = 1;
 
-        public void print()
-        {
+        public void print(){
             System.Console.WriteLine("     Color Coding Manual     ");
             System.Console.WriteLine("Number  Major Color  Minor Color");
-            for (int i = 0; i < totalMajorColor; i++)
-            {
-                for (int j = 0; j < totalMinorColor; j++)
-                {
+            for (int i = 0; i < totalMajorColor; i++){
+                for (int j = 0; j < totalMinorColor; j++){
                     Console.WriteLine(" {0}        {1}          {2}", numricValueOfColorPair, ColorMap.colorMapMajor[i].Name, ColorMap.colorMapMinor[j].Name);
                     numricValueOfColorPair++;
                 }
             }
         }
     }
-
-
 }
+
